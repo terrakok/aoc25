54dfff3b4917c2694c58c4fdc4e77b9e5011c93d
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2025-12-05 11:59:30 +0100

    day 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package org.example
 import java.io.File
 
 fun main() {
-    aoc4_2()
+    aoc5_2()
 }
 
 fun aoc1_1() {
@@ -184,6 +184,46 @@ fun aoc4_2() {
             removed.forEach { (c, r) -> data[c][r] = false }
         }
     }
+
+    println("Result: $result")
+}
+
+fun aoc5_1() {
+    val file = File("./data/5/input.txt")
+    val data = file.readLines()
+    val empty = data.indexOfFirst { it.isEmpty() }
+    val ranges = data.subList(0, empty).map {
+        val (a, b) = it.split('-').map { it.toLong() }
+        a..b
+    }
+    val products = data.subList(empty + 1, data.size).map { it.toLong() }
+
+    val result = products.filter { p -> ranges.any { p in it } }.count()
+
+    println("Result: $result")
+}
+
+fun aoc5_2() {
+    val file = File("./data/5/input.txt")
+    val data = file.readLines()
+    val empty = data.indexOfFirst { it.isEmpty() }
+    val ranges = data.subList(0, empty).map {
+        val (a, b) = it.split('-').map { it.toLong() }
+        a..b
+    }.sortedBy { it.first }
+
+    val joined = mutableListOf(ranges.first())
+    ranges.forEach { r ->
+        val j = joined.last()
+        if (j.last in r) {
+            joined.removeLast()
+            joined.add(j.first..r.last)
+        } else if (j.last < r.first) {
+            joined.add(r)
+        }
+    }
+
+    val result = joined.sumOf { it.last - it.first + 1 }
 
     println("Result: $result")
 }
