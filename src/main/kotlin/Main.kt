edf42f53bce38ab184195ada72c80b0f67067032
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2025-12-06 15:48:17 +0100

    day 6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package org.example
 import java.io.File
 
 fun main() {
-    aoc5_2()
+    aoc6_2()
 }
 
 fun aoc1_1() {
@@ -226,4 +226,69 @@ fun aoc5_2() {
     val result = joined.sumOf { it.last - it.first + 1 }
 
     println("Result: $result")
+}
+
+fun aoc6_1() {
+    val file = File("./data/6/input.txt")
+    val lines = file.readLines()
+    val data = lines.dropLast(1)
+    val op = lines.last()
+
+    val n = data.map { it.split(' ').map { it.trim() }.filter { it.isNotEmpty() } }
+    val maxLine = n.maxBy { it.size }.size
+
+    var d = 0
+    var res = 0L
+    repeat(maxLine) { r ->
+        val nums = mutableListOf<String>()
+        repeat(n.size) { c ->
+            n.getOrNull(c)?.let { it.getOrNull(r) }?.let { nums.add(it) }
+        }
+        val l = nums.maxOf { it.length }
+        val isPlus = op.substring(d, d + l).contains("+")
+        d += l + 1
+        res += nums.map { it.toLong() }.reduce { acc, i -> if (isPlus) acc + i else acc * i }
+    }
+
+    println("Result: $res")
+}
+
+fun aoc6_2() {
+    val file = File("./data/6/input.txt")
+    val lines = file.readLines()
+    val w = lines.first().length
+    val h = lines.size
+
+    val rotatedLines = mutableListOf<String>()
+    repeat(w) { c ->
+        val s = w - c - 1
+        val str = buildString {
+            repeat(h) { a -> append(lines[a][s]) }
+        }
+        rotatedLines.add(str)
+    }
+    rotatedLines.add("   ")
+
+
+    var res = 0L
+    var nums = mutableListOf<String>()
+    rotatedLines.forEach { line ->
+        if (line.isBlank()) {
+            var isSum = true
+            val longs = nums.map { n ->
+                if (n.last() == ' ') {
+                    n.trim().toLong()
+                } else {
+                    isSum = n.last() == '+'
+                    n.take(n.length - 1).trim().toLong()
+                }
+            }
+            res += longs.reduce { acc, i -> if (isSum) acc + i else acc * i }
+            nums = mutableListOf()
+        } else {
+            nums.add(line)
+        }
+    }
+
+    println("Result: $res")
 }
