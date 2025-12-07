b569a811a7468f239222fb61bc09cfcc4254cec0
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2025-12-07 10:22:03 +0100

    day 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 package org.example
 
 import java.io.File
+import kotlin.math.cos
 
 fun main() {
-    aoc6_2()
+    aoc7_2()
 }
 
 fun aoc1_1() {
@@ -290,5 +291,65 @@ fun aoc6_2() {
         }
     }
 
+    println("Result: $res")
+}
+
+fun aoc7_1() {
+    val file = File("./data/7/input.txt")
+    val lines = file.readLines()
+
+    val s = lines.first().indexOf('S')
+
+    val flows = mutableSetOf(s)
+    var splits = 0
+    lines.forEach { l ->
+        l.forEachIndexed { i, c ->
+            if (c == '^' && i in flows) {
+                flows.remove(i)
+                flows.add(i - 1)
+                flows.add(i + 1)
+                splits++
+            }
+        }
+    }
+
+    println("Result: $splits")
+}
+
+fun aoc7_2() {
+    val file = File("./data/7/input.txt")
+    val lines = file.readLines()
+
+    val w = lines.first().length
+    val s = lines.first().indexOf('S')
+
+    val flows = mutableSetOf(s)
+    val timelines = mutableMapOf(s to 1L)
+
+    lines.forEach { l ->
+        l.forEachIndexed { i, c ->
+            if (c == '^' && i in flows) {
+                require(flows == timelines.keys)
+
+                val count = timelines[i]!!
+
+                flows.remove(i)
+                timelines.remove(i)
+
+                if (i > 0) {
+                    flows.add(i - 1)
+                    val new = timelines.getOrDefault(i - 1, 0L) + count
+                    timelines[i - 1] = new
+                }
+                if (i < w - 1) {
+                    flows.add(i + 1)
+                    val new = timelines.getOrDefault(i + 1, 0L) + count
+                    timelines[i + 1] = new
+                }
+            }
+        }
+    }
+
+    val res = timelines.values.sum()
     println("Result: $res")
 }
